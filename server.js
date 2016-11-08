4e07b5b7909ab88ce28ecc813b4f266248283f1a
Ziqul <m.s.v.00a@gmail.com>
2016-11-08 13:14:05 +0200

    Setting server and connection to db

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 'use strict';
 
+const path = require('path');
 const config = require('./config.js');
 const express = require('express');
 const app = express();
 const server = app.listen(config.server.port, function() {
-	console.log('Server listening at' + config.server.protocol
+	console.log('Server listening at ' + config.server.protocol
 				+ config.server.adress + ':' + config.server.port);
 });
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://' + config.db.adress
+				+ ':' + config.db.port
+				+ '/' +config.db.name);
 
 app.use(express.static(path.join(__dirname+'/public')));
 

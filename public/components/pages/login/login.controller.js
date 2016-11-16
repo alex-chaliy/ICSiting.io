95901cdbeea55ac9aa2bbcbafd41e42be6187067
Alex Chaliy <alex.chaliy.42@gmail.com>
2016-11-16 20:09:43 +0200

    added login page

--- public/components/pages/login/login.controller.js
+++ public/components/pages/login/login.controller.js
@@ -1,6 +1,19 @@
 'use strict';
 
 let loginController = ($scope, $http, $location) => {
+	$scope.login = (userData) => {
+		$http({
+			method: 'POST',
+			url: '/login',
+			data: userData
+		})
+		.success((response) => {
+			console.log('Login status: Success.');
+		})
+		.error(() => {
+			console.log('Cannot login.');
+		});
+	}
 }
 loginController.$inject = [
 	'$scope',
